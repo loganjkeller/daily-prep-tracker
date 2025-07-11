9e75f60e5a30a7f306beb7274ecbbc84d66657d7
Logan Keller Battistinii <75399270+loganjkeller@users.noreply.github.com>
2025-07-11 15:11:04 +0000

    Initial commit for cafe preparation app add item

--- cafepreparation.py
+++ cafepreparation.py
@@ -31,7 +31,7 @@ PRODUCTS = [
     "Croissant Chocolate", "Croissant Pistachio", "Croissant Plain", "Apple turn over", "Multigrain Muffin",
     "Chocolate chip Muffin", "Caprese sandwich", "Egg salad Sandwich", "Prosciutto Arugula Sandwich",
     "Chicken Cutlet Sandwich", "Maritozzo", "Mix Berry Tart", "Pizza", "Prosciutto Cheese Sandwich",
-    "Ham Cheese Sandwich", "Zucchini Pesto Sandwich", "Tiramisu", "Vegan Crostata",
+    "Ham Cheese Sandwich", "Zucchini Pesto Sandwich", "Tiramisu", "Vegan Crostata Tart",
 ]
 
 # === Google Sheets Helper Functions ===
